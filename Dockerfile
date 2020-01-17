f09df404af62ed0893ee2ca3431c89eb1c319899
Sorunome <mail@sorunome.de>
2020-01-17 19:49:07 +0100

    udpate dockerfiles

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,28 @@
+FROM node:latest AS builder
+
+WORKDIR /opt/mx-puppet-slack
+RUN adduser --disabled-password --gecos '' builder \
+ && chown builder:builder .
+USER builder
+
+COPY package.json package-lock.json ./
+RUN npm install
+
+COPY tsconfig.json ./
+COPY src/ ./src/
+RUN npm run build
+
+
 FROM node:alpine
 
-COPY . /opt/mx-puppet-slack
-WORKDIR /opt/mx-puppet-slack
-RUN apk add --no-cache ca-certificates \
-	&& apk add --no-cache --virtual .build-deps git make gcc g++ python \
-	&& npm install \
-	&& npm run build \
-	&& apk del .build-deps
 VOLUME ["/data"]
+
+RUN adduser -D -g '' bridge
+
+WORKDIR /opt/mx-puppet-slack
+
+COPY docker-run.sh ./
+COPY --from=builder /opt/mx-puppet-slack/node_modules/ ./node_modules/
+COPY --from=builder /opt/mx-puppet-slack/build/ ./build/
+
 ENTRYPOINT ["./docker-run.sh"]
--- docker-run.sh
+++ docker-run.sh
@@ -1,11 +1,22 @@
-#!/bin/sh
-if [ ! -f "/data/config.yaml" ]; then
-	echo "No config found"
+#!/bin/sh -e
+
+chown -R bridge:bridge /data
+
+if [ ! -f '/data/config.yaml' ]; then
+	echo 'No config found'
 	exit 1
 fi
-if [ ! -f "/data/slack-registration.yaml" ]; then
-	node /opt/mx-puppet-slack/build/index.js -c /data/config.yaml -f /data/slack-registration.yaml -r
-	echo "Registration generated."
+
+if [ ! -f '/data/slack-registration.yaml' ]; then
+    su -l bridge -c "/usr/local/bin/node '/opt/mx-puppet-slack/build/index.js' \
+            -c '/data/config.yaml' \
+            -f '/data/slack-registration.yaml' \
+            -r"
+
+	echo 'Registration generated.'
 	exit 0
 fi
-node /opt/mx-puppet-slack/build/index.js -c /data/config.yaml -f /data/slack-registration.yaml
+
+su -l bridge -c "/usr/local/bin/node '/opt/mx-puppet-slack/build/index.js' \
+    -c '/data/config.yaml' \
+    -f '/data/slack-registration.yaml'"
